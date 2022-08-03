84b150e3fc883f6447f14d154c81e0930c755c0a
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 14:51:40 +0100

    add useful log msgs

--- db/scripts/deleteHabits.js
+++ db/scripts/deleteHabits.js
@@ -3,7 +3,8 @@ import { query } from "../index.js";
 async function deleteHabitsTable() {
 	const SQL = `DROP TABLE IF EXISTS habits`;
 	const res = await query(SQL);
-	console.log(`${res.command}: deleted habits table`);
+	console.log(`${res.command} : habits table deleted`);
 }
 
+console.log("deleting habits table...");
 deleteHabitsTable();
