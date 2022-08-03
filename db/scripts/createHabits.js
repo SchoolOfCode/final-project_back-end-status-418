73e0f264a940c0b52a3c02f7556e436e631014c4
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 12:52:27 +0100

    Add check for freq_options enums type existing before attempting to create it

--- db/scripts/createHabits.js
+++ db/scripts/createHabits.js
@@ -1,25 +1,52 @@
 import { query } from "../index.js";
 // import "dotenv/config";
 
-//Create a custom data type for daily/weekly/monthly only
-const setFreqIntervalTypes =
-	"CREATE TYPE FREQ_OPTIONS as ENUM ('daily', 'weekly', 'monthly'); ";
+/**Check if FREQ_OPTIONS type exists in the habits table before creating it.
+FREQ_OPTIONS is a custom data type for daily/weekly/monthly */
 
-const SQL = `CREATE TABLE IF NOT EXISTS 
-                habits(
-                    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
-                    name TEXT, 
-                    description TEXT, 
-                    userId TEXT, 
-                    everyday BOOLEAN,
-                    frequency_reps SMALLINT,
-                    frequency_interval FREQ_OPTIONS,
-                    date_created TIMESTAMPTZ
-            );`;
+async function checkFreqOps() {
+	//define the frequency_interval options
+	const typeOptions = ["daily", "weekly", "monthly"];
 
-async function createHabitsTable() {
-	const res = await query(setFreqIntervalTypes + SQL);
-	console.log(res.command, ": created habits table");
+	const sql = `SELECT typname FROM pg_type WHERE typcategory = 'E'`;
+	const res = await query(sql);
+
+	const exists =
+		Object.fromEntries(
+			Object.entries(res.rows[0]).filter(
+				([key, value]) => value === "freq_options"
+			)
+		).typname === "freq_options";
+
+	if (!exists) {
+		const query = "CREATE TYPE FREQ_OPTIONS as ENUM ('$1', '$2', '$3');";
+		const res = await query(query, typeOptions);
+		console.log(`${res.command}: created type freq_options`);
+	} else {
+		console.log("Type freq_options already exists");
+	}
 }
 
+async function createHabitsTable() {
+	const SQL = `CREATE TABLE IF NOT EXISTS 
+    habits(
+        id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+        name TEXT, 
+        description TEXT, 
+        userId TEXT, 
+        everyday BOOLEAN,
+        frequency_reps SMALLINT,
+        frequency_interval FREQ_OPTIONS,
+        date_created TIMESTAMPTZ
+);`;
+	const res = await query(SQL);
+	console.log(res.command, ": habits table");
+}
+
+// async () => {
+// 	await checkFreqOps();
+// 	await createHabitsTable();
+// };
+
+checkFreqOps();
 createHabitsTable();
