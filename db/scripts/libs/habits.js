68e0ec4d61438ef4b70886eff1878dee9dd6e041
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 11:30:41 +0100

    add temp data for habits table

--- db/scripts/libs/habits.js
+++ db/scripts/libs/habits.js
@@ -1,1 +1,32 @@
-const habits = [{}];
+//fr_quantity ?? fr_count?? fr_period ?? fr_reps
+
+const habits = [
+	{
+		id: 1,
+		name: "Walk the dog",
+		description: "Every day after work I'll grab the dog's leash",
+		userId: "abc123",
+		everyday: true,
+		frequency: { fr_reps: null, fr_interval: null },
+		date_created: new Date(),
+	},
+	{
+		id: 2,
+		name: "Drink water",
+		description: "Keep a jug full of water by my desk",
+		userId: "xyz123",
+		everyday: false,
+		frequency: { fr_reps: 8, fr_interval: "daily" },
+		date_created: new Date(),
+	},
+	{
+		id: 3,
+		name: "Go to gym",
+		description:
+			"On Mon, Wed and Fri I'll leave my gym bag by the front door",
+		userId: "abc123",
+		everyday: false,
+		frequency: { fr_reps: 3, fr_interval: "weekly" },
+		date_created: new Date(),
+	},
+];
