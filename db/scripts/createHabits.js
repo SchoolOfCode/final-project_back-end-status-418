4063fddc29a2b6d9e5bab904248e4122527fbaae
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 11:56:23 +0100

    add create habits table function

--- db/scripts/createHabits.js
+++ db/scripts/createHabits.js
@@ -3,22 +3,23 @@ import { query } from "../index.js";
 
 //Create a custom data type for daily/weekly/monthly only
 const setFreqIntervalTypes =
-	"CREATE TYPE freq_options as ENUM ('daily', 'weekly', 'monthly');";
+	"CREATE TYPE FREQ_OPTIONS as ENUM ('daily', 'weekly', 'monthly'); ";
+
+const SQL = `CREATE TABLE IF NOT EXISTS 
+                habits(
+                    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+                    name TEXT, 
+                    description TEXT, 
+                    userId TEXT, 
+                    everyday BOOLEAN,
+                    frequency_reps SMALLINT,
+                    frequency_interval FREQ_OPTIONS,
+                    date_created TIMESTAMPTZ
+            );`;
 
 async function createHabitsTable() {
-	await query(
-		`${setFreqIntervalTypes} 
-        CREATE TABLE IF NOT EXISTS 
-        habits(
-            id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
-            name TEXT, 
-            description TEXT, 
-            userId TEXT, 
-            everyday BOOLEAN,
-            frequency_reps SMALLINT
-            frequency_interval setFreqIntervalTypes
-            date_created TIMESTAMPTZ
-	    );`
-	);
+	const res = await query(setFreqIntervalTypes + SQL);
+	console.log(res.command, ": created habits table");
 }
+
 createHabitsTable();
