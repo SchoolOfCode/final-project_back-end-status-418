3363d162545cc717416572be0f1a5cd0f16aca8e
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 10:40:52 +0100

    initalise back end w express

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+	res.send("Welcome to Status 418’s Rootine back end");
+});
+
+// app.use("/users", usersRouter);
+// app.use("/habits", habitsRouter);
+// app.use("/calendar", calendarRouter);
+
+if (process.env.NODE_ENV != "test") {
+	app.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}`);
+	});
+}
+
+export default app;
