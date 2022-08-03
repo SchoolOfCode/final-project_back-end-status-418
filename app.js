05c7dc48f4ad77fff678da74eedf93946693bc8c
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 10:59:41 +0100

    add logger & helmet

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
-<<<<<<< HEAD
+import helmet from "helmet";
+import logger from "morgan";
 
 const PORT = 3001;
 const app = express();
 
+app.use(logger("dev"));
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
@@ -23,22 +26,3 @@ if (process.env.NODE_ENV != "test") {
 }
 
 export default app;
-=======
-import helmet from "helmet";
-import logger from "morgan";
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(logger("dev"));
-app.use(cors());
-app.use(helmet());
-
-app.get("/", (req, res) => {
-  res.send("Hello from root route");
-});
-
-app.listen(PORT, () => {
-  console.log(`listening to PORT ${PORT}`);
-});
->>>>>>> 973ad3969135ba2bcd595c69aefd7e91d30c65d4
