a3e768f9ddfef7f9efb20848a579be5db995face
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-03 14:50:23 +0100

    create fn to populate the habits table with temp data

---
+++ db/scripts/populateHabits.js
@@ -0,0 +1,22 @@
+import { query } from "../index.js";
+import { habits } from "./libs/habits.js";
+
+async function populateHabitsTable() {
+	let res;
+	for (let i = 0; i < habits.length; i++) {
+		const sql = `INSERT INTO habits (name, description, userId, everyday, frequency_reps, frequency_interval) VALUES ($1, $2, $3, $4, $5, $6)`;
+		const data = [
+			habits[i].name,
+			habits[i].description,
+			habits[i].userId,
+			habits[i].everyday,
+			habits[i].frequency.fr_reps,
+			habits[i].frequency.fr_interval,
+		];
+		res = await query(sql, data);
+	}
+	console.log(`${res.command} : populated habits table`);
+}
+
+console.log("populating habits table...");
+populateHabitsTable();
