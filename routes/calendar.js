65336b6a0915d12418518cce2e14bbbdb5544a8a
Samantha Wu <samanthayywu@gmail.com>
2022-08-04 15:42:34 +0100

    try catch get

--- routes/calendar.js
+++ routes/calendar.js
@@ -5,15 +5,25 @@ import { getAllByIDAndDate } from "../models/calendar.js";
 
 // get where habit_id and date is the same
 calendarRouter.get("/:id", async function (req, res) {
-  res.json({
-    success: true,
-    payload: await getAllByIDAndDate(Number(req.params.id)),
-  });
+  try {
+    res.status(200).json({
+      success: true,
+      payload: await getAllByIDAndDate(Number(req.params.id)),
+    });
+  } catch (e) {
+    res.status(404).send({
+      sucess: false,
+      message: "Please enter correct id number",
+    });
+  }
 });
 
 // update status
 
-// post
+// post new calendar entry
+calendarRouter.post("/", async function (req, res) {
+  res.json({ sucess: true });
+});
 
 // habit
 
