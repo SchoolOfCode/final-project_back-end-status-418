16d4d633dd97cd5de8b720cd98a29896b1a80b04
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-08-04 10:46:06 +0100

    add all habits and habits by id routes and models

--- models/habits.js
+++ models/habits.js
@@ -5,3 +5,9 @@ export async function getAllHabits() {
 	const res = await query("SELECT * FROM habits");
 	return res.rows;
 }
+
+/** Return habit by its id */
+export async function getHabitById(id) {
+	const res = await query("SELECT * FROM habits WHERE habits.id = $1;", [id]);
+	return res.rows;
+}
