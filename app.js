c1c918b7f2f6698bf2c7a662eb5264d6561a934e
Samantha Wu <samanthayywu@gmail.com>
2022-08-04 14:51:33 +0100

    updated calendar table

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import logger from "morgan";
 
 import habitsRouter from "./routes/habits.js";
+import calendarRouter from "./routes/calendar.js";
 
 const PORT = 3001;
 const app = express();
@@ -19,7 +20,7 @@ app.get("/", function (req, res) {
 
 // app.use("/users", usersRouter);
 app.use("/habits", habitsRouter);
-// app.use("/calendar", calendarRouter);
+app.use("/calendar", calendarRouter);
 
 if (process.env.NODE_ENV != "test") {
   app.listen(PORT, () => {
---
+++ routes/calendar.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+
+// to link tables
+// habit_id and date - primary key?
+// date_updated to evenutally change when status is changed
+
+// get where habit_id and date is the same
+router.get("/", (res, req) => {});
+
+// update status
+
+// post
+
+// habit
+
+export default calendarRouter;
