ab5cb3562dee5072df26c969d4df61bf8742c222
Samantha Wu <samanthayywu@gmail.com>
2022-08-05 10:32:31 +0100

    getByIDAndDate

--- models/calendar.js
+++ models/calendar.js
@@ -10,10 +10,20 @@ export async function getAllByID(id) {
   return res.rows;
 }
 
+export async function getAllByIDAndDate(id, date) {
+  const res = await query(
+    `SELECT * FROM calendar INNER JOIN habits
+        ON calendar.habit_id = habits.id
+        WHERE habit_id = $1 AND date = $2`,
+    [id, date]
+  );
+  return res.rows[0];
+}
+
 export async function newCalendarEntry(habitItem) {
   const { habit_id, date, status } = habitItem;
   const res = await query(
-    `INSERT INTO calendar(habit_id, date, status) VALUES ($1, $2, $3) RETURNING *`,
+    `INSERT INTO calendar(habit_id, date, status) VALUES ($1, $2, $3) RETURNING * ON CONFLICT DO NOTHING`,
     [habit_id, date, status]
   );
   return res.rows[0];
--- routes/calendar.js
+++ routes/calendar.js
@@ -1,9 +1,13 @@
 import express from "express";
 const calendarRouter = express.Router();
 
-import { getAllByID, newCalendarEntry } from "../models/calendar.js";
+import {
+  getAllByID,
+  getAllByIDAndDate,
+  newCalendarEntry,
+} from "../models/calendar.js";
 
-// get where habit_id
+// get all habits with a particular habit_id
 calendarRouter.get("/:id", async function (req, res) {
   try {
     res.status(200).json({
@@ -11,11 +15,20 @@ calendarRouter.get("/:id", async function (req, res) {
       payload: await getAllByID(Number(req.params.id)),
     });
   } catch (e) {
+    console.log(e);
     res.status(404).send({
       sucess: false,
       message: "Please enter correct id number",
     });
   }
+});
+
+// get habit by id and date
+calendarRouter.get("/:id", async function (req, res) {
+  res.json({
+    success: true,
+    payload: await getAllByIDAndDate(Number(req.params.id), req.query.date),
+  });
 });
 
 // update status
