ce57a266f587805b2bf9e14ac7aa704c326a2605
Samantha Wu <samanthayywu@gmail.com>
2022-08-09 11:04:06 +0100

    status codes

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ import {
 
 // GET all users
 userRouter.get("/", async function (req, res) {
-  return res.json({ success: true, payload: await getAllUsers() });
+  return res.status(200).json({ success: true, payload: await getAllUsers() });
 });
 
 //GET specific users using the user_id
@@ -22,7 +22,7 @@ userRouter.get("/:id", async function (req, res) {
       payload: await getUserById(req.params.id),
     });
   } catch (e) {
-    res.status(200).send({
+    res.status(400).send({
       success: false,
       message: "Please enter correct account id number",
     });
@@ -31,12 +31,12 @@ userRouter.get("/:id", async function (req, res) {
 
 // post new user ID from Auth0
 userRouter.post("/", async (req, res) => {
-  res.json({ success: true, payload: await addNewUser(req.body) });
+  res.status(202).json({ success: true, payload: await addNewUser(req.body) });
 });
 
 // update username- to allow users to change username
 userRouter.patch("/:id", async (req, res) => {
-  res.json({
+  res.status(202).json({
     success: true,
     payload: await changeUsername(req.body.username, req.params.id),
   });
@@ -44,7 +44,9 @@ userRouter.patch("/:id", async (req, res) => {
 
 // delete user
 userRouter.delete("/:id", async (req, res) => {
-  res.json({ success: true, payload: await deleteUser(req.params.id) });
+  res
+    .status(200)
+    .json({ success: true, payload: await deleteUser(req.params.id) });
 });
 
 export default userRouter;
