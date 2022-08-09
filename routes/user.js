e5c3a28ed4b5f74484bbc3c848a84215418897ad
Samantha Wu <samanthayywu@gmail.com>
2022-08-09 11:24:35 +0100

    error handling

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,12 @@ import {
 
 // GET all users
 userRouter.get("/", async function (req, res) {
-  return res.status(200).json({ success: true, payload: await getAllUsers() });
+  try {
+    res.status(200).json({ success: true, payload: await getAllUsers() });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: "Server Unavailable" });
+  }
 });
 
 //GET specific users using the user_id
@@ -21,7 +26,7 @@ userRouter.get("/:id", async function (req, res) {
       success: true,
       payload: await getUserById(req.params.id),
     });
-  } catch (e) {
+  } catch (err) {
     res.status(400).send({
       success: false,
       message: "Please enter correct account id number",
@@ -31,22 +36,39 @@ userRouter.get("/:id", async function (req, res) {
 
 // post new user ID from Auth0
 userRouter.post("/", async (req, res) => {
-  res.status(202).json({ success: true, payload: await addNewUser(req.body) });
+  try {
+    res
+      .status(202)
+      .json({ success: true, payload: await addNewUser(req.body) });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: "Server Unavailable" });
+  }
 });
 
 // update username- to allow users to change username
 userRouter.patch("/:id", async (req, res) => {
-  res.status(202).json({
-    success: true,
-    payload: await changeUsername(req.body.username, req.params.id),
-  });
+  try {
+    res.status(202).json({
+      success: true,
+      payload: await changeUsername(req.body.username, req.params.id),
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: "Server Unavailable" });
+  }
 });
 
 // delete user
 userRouter.delete("/:id", async (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, payload: await deleteUser(req.params.id) });
+  try {
+    res
+      .status(200)
+      .json({ success: true, payload: await deleteUser(req.params.id) });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: "Server Unavailable" });
+  }
 });
 
 export default userRouter;
